5e05d82031d5e094b8a6057016ba6c7e06162cef
Jorge Gabitto <jgabitto1792@gmail.com>
2021-11-28 19:49:41 -0600

    Add table skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import CryptoTable from "./components/Table";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <CryptoTable />
     </div>
   );
 }
